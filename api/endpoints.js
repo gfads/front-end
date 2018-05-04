3927b14aba40def07509b32c444bf477ac7a952b
Matheus Santana <embs@cin.ufpe.br>
2018-05-04 18:37:53 -0300

    Change services URLs config

--- api/endpoints.js
+++ api/endpoints.js
@@ -17,12 +17,12 @@
   module.exports = {
     catalogueUrl:  util.format("http://catalogue%s", domain),
     tagsUrl:       util.format("http://catalogue%s/tags", domain),
-    cartsUrl:      util.format("http://carts%s/carts", domain),
-    ordersUrl:     util.format("http://orders%s", domain),
-    customersUrl:  util.format("http://user%s/customers", domain),
-    addressUrl:    util.format("http://user%s/addresses", domain),
-    cardsUrl:      util.format("http://user%s/cards", domain),
-    loginUrl:      util.format("http://user%s/login", domain),
-    registerUrl:   util.format("http://user%s/register", domain),
+    cartsUrl:      util.format("http://carts:8080%s/carts", domain),
+    ordersUrl:     util.format("http://orders-front%s", domain),
+    customersUrl:  util.format("http://user:8080%s/customers", domain),
+    addressUrl:    util.format("http://user:8080%s/addresses", domain),
+    cardsUrl:      util.format("http://user:8080%s/cards", domain),
+    loginUrl:      util.format("http://user:8080%s/login", domain),
+    registerUrl:   util.format("http://user:8080%s/register", domain),
   };
 }());
